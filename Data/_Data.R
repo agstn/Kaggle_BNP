43250ae84b27d9047927b39e1de2109a120fa122
agstn <acalatroni@gmail.com>
2016-03-16 03:43:59 +0000

    New Data Files

--- Data/_Data.R
+++ Data/_Data.R
@@ -3,12 +3,13 @@ if (Sys.info()['sysname']=="Windows") {
   loc_in   <- "C:/Git/Kaggle_BNP/Data/Raw"
   loc_out  <- "C:/Git/Kaggle_BNP/Data/Derive"
 } else {
-  loc_in   <- "/home/ubuntu/github/AstraZeneca-Sanger/Results/Final/BASE2"
-  loc_out  <- "/home/ubuntu/github/AstraZeneca-Sanger/Results/Final/BASE2"
+  loc_in   <- "~/Kaggle_BNP/Data/Raw"
+  loc_out  <- "/home/acalatroni/Kaggle_BNP/Data/Derive"
 }
 
 #' libraries
-pacman::p_load(h2o,h2oEnsemble,readr,dplyr)
+pacman::p_load(pacman)
+p_load(h2o,h2oEnsemble,readr,dplyr)
 
 #' Import data
 test  <- read_csv(paste0(loc_in,"/test.csv.zip"))
@@ -19,81 +20,22 @@ comb <- rbind(train[,-2],test)
 
 #' Remove Near Zero
 nzv <- caret::nearZeroVar(comb)
+comb <- comb[,-nzv]
 
-#' Start h2o
-h2o.init(nthreads=-1)
+#' Create Factors
+l1 <- apply(comb,2,function(x) length(unique(x)))
+comb[,which(l1<10)] <- lapply(comb[,which(l1<10)], as.factor)
 
-#' Import Data
-comb_h2o <- as.h2o(comb)
+#' Converts characters to factors
+comb[sapply(comb, is.character)] <- lapply(comb[sapply(comb, is.character)],as.factor)
 
-#' Impute Median/Mode
-for (i in 2:132){
-  if(is.factor(comb_h2o[,i])=="FALSE") h2o.impute(data = comb_h2o, column = i, method = "median")
-  if(is.factor(comb_h2o[,i])=="TRUE")  h2o.impute(data = comb_h2o, column = i, method = "mode")
-}
+#' Impute median/mode
+comb <- randomForest::na.roughfix(comb)
 
-train <- h2o.uploadFile(paste0(loc_in,"/train.csv"), destination_frame = "train.hex")
+#' Export
+train2 <- comb[1:nrow(train),]
+train2$target <- train$target
+saveRDS(train2,paste0(loc_out,"/train.rds"))
 
-train2 <- h2o.splitFrame(train, 0.8, destination_frames=c("trainSplit","testSplit"))
-train2 <- train2[[2]]
-train <- train2
-#test  <- h2o.uploadFile(paste0(loc_in,"/test.csv"),  destination_frame = "test.hex")
-#
-
-
-#' create factor
-train$target<-as.factor(train$target)
-
-
-#' Impute Median/Mode
-for (i in 3:133){
-  if(is.factor(train[,i])=="FALSE") h2o.impute(data = train, column = i, method = "median")
-  if(is.factor(train[,i])=="TRUE")  h2o.impute(data = train, column = i, method = "mode")
-  print(i)
-}
-
-#' Impute Median/Mode
-# for (i in 2:133){
-#   if(is.factor(train[,i])=="FALSE") h2o.impute(data = test, column = i, method = "median")
-#   if(is.factor(train[,i])=="TRUE")  h2o.impute(data = test, column = i, method = "mode")
-#   print(i)
-# }
-
-#' Splits
-splits = h2o.splitFrame(train, 0.9, destination_frames=c("trainSplit","testSplit"))
-
-#' Setup
-y      <- "target"
-x      <- setdiff(names(train[,-1]), y)
-family <- "binomial"
-
-#' Specify the base learner & the metalearner
-learner <- c("h2o.glm.wrapper",
-             "h2o.randomForest.wrapper",
-             "h2o.gbm.wrapper",
-             "h2o.deeplearning.wrapper")
-
-metalearner <- "h2o.glm.wrapper"
-
-#' Ensemble training
-fit <- h2o.ensemble(x = x,
-                   y = y,
-                   training_frame   = splits[[1]],
-                   validation_frame = splits[[2]],
-                   family = "binomial",
-                   learner = learner,
-                   metalearner = metalearner,
-                   cvControl = list(V=5)
-)
-
-perf <- h2o.ensemble_performance(fit, newdata = splits[[2]])
-
-#' Predict
-p <- predict(fit, splits[[2]])
-labels = as.data.frame(splits[[2]][,"target"])
-
-# p <- as.data.frame(h2o.predict(gbm,test))
-# testIds <- as.data.frame(test$ID)
-# submission <- data.frame(cbind(testIds,p$p1))
-# colnames(submission)<-c("ID","PredictedProb")
-# write.csv(submission,"submission.csv",row.names=F)
+test2  <- comb[(nrow(train)+1):nrow(comb),]
+saveRDS(test2,paste0(loc_out,"/test.RDS"))
